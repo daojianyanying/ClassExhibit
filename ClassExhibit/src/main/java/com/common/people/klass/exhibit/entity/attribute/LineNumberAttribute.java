7cb21a06e539e1f9e1f436d453f87ba7ce0e7cbd
daojianyanying <liu3574226@163.com>
2021-01-31 00:40:55 +0800

    属性类-LineNumberAttribute.java创建

---
+++ ClassExhibit/src/main/java/com/common/people/klass/exhibit/entity/attribute/LineNumberAttribute.java
@@ -0,0 +1,46 @@
+package com.common.people.klass.exhibit.entity.attribute;
+
+import java.util.ArrayList;
+
+public class LineNumberAttribute implements AttributeInfo {
+    private String nameIndex;
+    private Integer length;
+    private Integer lineNumberTableLength;
+    private ArrayList<LineNumberTable> lineNumberTables;
+
+    public LineNumberAttribute setNameIndex(String nameIndex) {
+        this.nameIndex = nameIndex;
+        return this;
+    }
+
+    public LineNumberAttribute setLength(Integer length) {
+        this.length = length;
+        return this;
+    }
+
+    public LineNumberAttribute setLineNumberTableLength(Integer lineNumberTableLength) {
+        this.lineNumberTableLength = lineNumberTableLength;
+        return this;
+    }
+
+    public LineNumberAttribute setLineNumberTables(ArrayList<LineNumberTable> lineNumberTables) {
+        this.lineNumberTables = lineNumberTables;
+        return this;
+    }
+
+    public String getNameIndex() {
+        return nameIndex;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public Integer getLineNumberTableLength() {
+        return lineNumberTableLength;
+    }
+
+    public ArrayList<LineNumberTable> getLineNumberTables() {
+        return lineNumberTables;
+    }
+}
