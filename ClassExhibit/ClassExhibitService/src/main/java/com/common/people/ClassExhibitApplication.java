6982880938fc3e9632f6e4b52ffe75ce796125cc
daojianyanying <liu3574226@163.com>
2021-02-06 20:23:00 +0800

    【service模块更改】改为SpringBoot的项目结构

---
+++ ClassExhibit/ClassExhibitService/src/main/java/com/common/people/ClassExhibitApplication.java
@@ -0,0 +1,11 @@
+package com.common.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassExhibitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClassExhibitApplication.class);
+    }
+}
